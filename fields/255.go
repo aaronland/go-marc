90741e12c58aff6757d5fcb93ab45582fcca0339
sfomuseumbot <sfomuseumbot@localhost>
2025-03-02 12:12:00 -0800

    snapshot: start blocking out code/notes for 255 support

---
+++ fields/255.go
@@ -0,0 +1,11 @@
+package fields
+
+// https://www.loc.gov/marc/bibliographic/bd255.html
+// https://web.archive.org/web/20100611194617/https://www.acsu.buffalo.edu/~dbertuca/maps/cat/fields.html
+// https://web.archive.org/web/20100610163109/http://library.buffalo.edu/maps/mapresources/map_cat_tools.php
+
+func init() {
+
+	
+}
+
