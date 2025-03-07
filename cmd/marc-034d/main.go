8e6ca90277a45d485b47b2e9b00403bd4a57230e
sfomuseumbot <sfomuseumbot@localhost>
2025-03-07 15:21:52 -0800

    update to use aaronland/go-http-maps/v2

--- cmd/marc-034d/main.go
+++ cmd/marc-034d/main.go
@@ -7,15 +7,13 @@ import (
 	"log"
 	"log/slog"
 	gohttp "net/http"
-	"net/url"
 	"os"
-	"strings"
 
 	"github.com/aaronland/go-http-server"
+	"github.com/aaronland/go-http-maps/v2"	
 	"github.com/aaronland/go-marc/v2/http"
 	"github.com/aaronland/go-marc/v2/static/www"
 	"github.com/sfomuseum/go-flags/flagset"
-	"github.com/sfomuseum/go-http-protomaps"
 )
 
 const leaflet_osm_tile_url = "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -24,28 +22,27 @@ const protomaps_api_tile_url string = "https://api.protomaps.com/tiles/v3/{z}/{x
 func main() {
 
 	var verbose bool
-
 	var server_uri string
+	var marc034_column string
+	
 	var map_provider string
 	var map_tile_uri string
 	var protomaps_theme string
-
-	var style string
-
-	var marc034_column string
+	var initial_view string
+	var leaflet_style string
+	var leaflet_point_style string	
 
 	fs := flagset.NewFlagSet("marc-034")
 
 	fs.StringVar(&map_provider, "map-provider", "leaflet", "Valid options are: leaflet, protomaps")
 	fs.StringVar(&map_tile_uri, "map-tile-uri", leaflet_osm_tile_url, "A valid Leaflet tile layer URI. See documentation for special-case (interpolated tile) URIs.")
 	fs.StringVar(&protomaps_theme, "protomaps-theme", "white", "A valid Protomaps theme label.")
-
-	fs.StringVar(&style, "style", "", "A custom Leaflet style definition for geometries. This may either be a JSON-encoded string or a path on disk.")
-
+	fs.StringVar(&leaflet_style, "leaflet-style", "", "A custom Leaflet style definition for geometries. This may either be a JSON-encoded string or a path on disk.")
+	fs.StringVar(&leaflet_point_style, "leaflet-point-style", "", "A custom Leaflet style definition for point geometries. This may either be a JSON-encoded string or a path on disk.")	
+	fs.StringVar(&initial_view, "initial-view", "", "A comma-separated string indicating the map's initial view. Valid options are: 'LON,LAT', 'LON,LAT,ZOOM' or 'MINX,MINY,MAXX,MAXY'.")
+	
 	fs.StringVar(&marc034_column, "marc034-column", "marc_034", "The name of the CSV column where MARC 034 data is stored.")
-
 	fs.StringVar(&server_uri, "server-uri", "http://localhost:8080", "A valid aaronland/go-http-server URI.")
-
 	fs.BoolVar(&verbose, "verbose", false, "Enable verbose (debug) logging.")
 
 	fs.Usage = func() {
@@ -71,6 +68,21 @@ func main() {
 
 	mux := gohttp.NewServeMux()
 
+	opts := &maps.AssignMapConfigHandlerOptions{
+		MapProvider:       map_provider,
+		MapTileURI:        map_tile_uri,
+		InitialView:       initial_view,
+		LeafletStyle:      leaflet_style,
+		LeafletPointStyle: leaflet_point_style,
+		ProtomapsTheme:    protomaps_theme,
+	}
+
+	err = maps.AssignMapConfigHandler(opts, mux, "/map.json")
+
+	if err != nil {
+		log.Fatalf("Failed to assign map config handler, %v", err)
+	}
+	
 	bbox_handler, err := http.BboxHandler()
 
 	if err != nil {
@@ -90,52 +102,6 @@ func main() {
 	}
 
 	mux.Handle("/convert", convert_handler)
-
-	// START OF put me in a function or something...
-
-	map_cfg := &http.MapConfig{
-		Provider: map_provider,
-		TileURL:  map_tile_uri,
-		// Style:           style,
-	}
-
-	if map_provider == "protomaps" {
-
-		u, err := url.Parse(map_tile_uri)
-
-		if err != nil {
-			log.Fatalf("Failed to parse Protomaps tile URL, %v", err)
-		}
-
-		switch u.Scheme {
-		case "file":
-
-			mux_url, mux_handler, err := protomaps.FileHandlerFromPath(u.Path, "")
-
-			if err != nil {
-				log.Fatalf("Failed to determine absolute path for '%s', %v", map_tile_uri, err)
-			}
-
-			mux.Handle(mux_url, mux_handler)
-			map_cfg.TileURL = mux_url
-
-		case "api":
-			key := u.Host
-			map_cfg.TileURL = strings.Replace(protomaps_api_tile_url, "{key}", key, 1)
-		}
-
-		map_cfg.Protomaps = &http.ProtomapsConfig{
-			Theme: protomaps_theme,
-		}
-	}
-
-	// END OF put me in a function or something...
-
-	map_cfg_handler := http.MapConfigHandler(map_cfg)
-
-	mux.Handle("/map.json", map_cfg_handler)
-
-	//
 
 	www_fs := gohttp.FS(www.FS)
 	www_handler := gohttp.FileServer(www_fs)
--- go.mod
+++ go.mod
@@ -3,25 +3,18 @@ module github.com/aaronland/go-marc/v2
 go 1.24.0
 
 require (
+	github.com/aaronland/go-http-maps/v2 v2.0.0
 	github.com/aaronland/go-http-sanitize v0.0.8
 	github.com/aaronland/go-http-server v1.5.0
 	github.com/paulmach/orb v0.11.1
 	github.com/sfomuseum/go-csvdict/v2 v2.0.1
 	github.com/sfomuseum/go-flags v0.10.0
-	github.com/sfomuseum/go-http-protomaps v0.3.0
 )
 
 require (
-	github.com/aaronland/go-http-leaflet v0.5.0 // indirect
-	github.com/aaronland/go-http-rewrite v1.1.1 // indirect
-	github.com/aaronland/go-http-static v0.0.3 // indirect
 	github.com/aaronland/go-roster v1.0.0 // indirect
 	github.com/akrylysov/algnhsa v1.1.0 // indirect
 	github.com/aws/aws-lambda-go v1.47.0 // indirect
-	github.com/sfomuseum/go-http-rollup v0.0.3 // indirect
-	github.com/tdewolff/minify/v2 v2.20.32 // indirect
-	github.com/tdewolff/parse/v2 v2.7.14 // indirect
 	github.com/whosonfirst/go-sanitize v0.1.0 // indirect
 	go.mongodb.org/mongo-driver v1.11.4 // indirect
-	golang.org/x/net v0.33.0 // indirect
 )
